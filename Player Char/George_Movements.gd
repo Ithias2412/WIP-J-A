8ee6e4635452b8ac4ee0019ded3cf00fd4217f50
Ithias <ALDieguez9@gmail.com>
2024-03-06 05:32:48 -0600

    Made George groove!

--- Player Char/George_Movements.gd
+++ Player Char/George_Movements.gd
@@ -10,3 +10,19 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	
+#This is all my funny anim code, I dont know what i'm doing :(
+@onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
+
+func _process(delta):
+	if abs(velocity.x) > 10:
+		animated_sprite_2d.play("George_Move")
+	else:
+		if abs(velocity.y) > 10:
+			animated_sprite_2d.play("George_Move")
+		else:
+			animated_sprite_2d.play("George_Idle")
+	if velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+	if velocity.x > 0:
+		animated_sprite_2d.flip_h = false
